197af01837cbb03e4cdc529d9be785555450f703
Issei Horie <issei.horie@nulab-inc.com>
2019-01-20 14:13:26 +0900

    Fix require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ require 'rspec'
 require 'rack/test'
 require 'webmock/rspec'
 require 'omniauth'
-require 'omniauth-typetalk'
+require 'omniauth_typetalk'
 
 RSpec.configure do |config|
   config.include WebMock::API
